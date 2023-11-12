7d85fb5bbf4a5ee4f7d1ddc44f24270d86929e0d
Harrison-git <kharisov037@gmail.com>
2023-11-13 01:01:30 +0500

    Tasks is complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println();
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+        String result = Main.calc(input);
+        System.out.println(result);
+    }
+
+    public static String calc(String input) throws Exception {
+        String[] inputs = input.split(" ");
+        if (inputs.length != 3) {
+            throw new Exception();
+        }
+        String operation = inputs[1];
+
+        int value1 = Integer.parseInt(inputs[0]);
+        int value2 = Integer.parseInt(inputs[2]);
+
+        int result;
+
+        switch (operation) {
+            case "+" -> result = value1 + value2;
+            case "-" -> result = value1 - value2;
+            case "*" -> result = value1 * value2;
+            case "/" -> result = value1 / value2;
+            default -> throw new Exception();
+
+        }
+        return String.valueOf(result);
     }
 }
